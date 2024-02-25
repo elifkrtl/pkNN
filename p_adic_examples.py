17e3079bf9560b489dc08a3fcd37d6a48f043a07
Elif Kartal <103775821+elifkrtl@users.noreply.github.com>
2024-02-25 20:44:22 +0300

    Update p_adic_examples.py

--- p_adic_examples.py
+++ p_adic_examples.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on January 24, 2023
+Created on February 16, 2024
 @authors: 
-    Elif KARTAL, Istanbul University, Faculty of Economics, Department of Management Information Systems 
-    Fatma CALISKAN, Istanbul University, Faculty Of Science, Department Of Mathematics
-    Beyaz Basak ESKISEHIRLI, Istanbul University, Faculty Of Science, Department Of Mathematics
+    Elif KARTAL, Istanbul University, Faculty of Economics, Department of Management Information Systems
+    Beyaz Basak ESKISEHIRLI, Faculty of Science, Department of Mathematics
+    Fatma CALISKAN, Faculty of Science, Department of Mathematics
     Zeki OZEN, Istanbul University, Faculty of Economics, Department of Management Information Systems
     
-@title: Examples with p-adic Values and p-adic Distance
+@title: Examples with p-adic calculations 
 """
 
 import p_adic
 
-# p-adic Values
-
 x = 25
 
-# p_adic_val() function returns p-adic value of a given number.
+# p_adic_val() function returns the p-adic absolute value of a given number.
 x_p2 = p_adic.p_adic_val(x, p_num=2)
 print("2-adic value of ", x, " is ", x_p2)
 
@@ -30,9 +28,10 @@ y = 16
 y_p2 = p_adic.p_adic_val(y, p_num=2)
 print("2-adic value of ", y, " is ", y_p2)
 
-# p_adic_pow_val() function returns both p-adic power and p-adic value.
-y_p2_pow, y_p2_val = p_adic.p_adic_pow_val(y, p_num=2)
-print(y_p2_pow)
+# p_adic_pow_val() function returns both the ord and the the p-adic absolute value of the given number.
+
+y_p2_ord, y_p2_val = p_adic.p_adic_pow_val(y, p_numb=2)
+print(y_p2_ord)
 print(y_p2_val)
 
 y_p3 = p_adic.p_adic_val(y, p_num=3)
@@ -57,76 +56,27 @@ print("3-adic value of ", p, " is ", p_adic.p_adic_val(p, p_num=3))
 print("5-adic value of ", p, " is ", p_adic.p_adic_val(p, p_num=5))
 print("17-adic value of ", p, " is ", p_adic.p_adic_val(p, p_num=17))
 
-r = -54
-print("3-adic value of ", r, " is ", p_adic.p_adic_val(r, p_num=3))
+r1 = -54
+print("3-adic value of ", r1, " is ", p_adic.p_adic_val(r1, p_num=3))
 
 
 s = -(24/16)
 print("2-adic value of ", s, " is ", p_adic.p_adic_val(s, p_num=2))
-print(p_adic.p_adic_pow_val(s, p_num=2))
 
+print(p_adic.p_adic_pow_val(s, p_numb=2))
+
+s1 = 8
+print(p_adic.p_adic_pow_val(s1, p_numb=2))
+
+s2 = 100
+print(p_adic.p_adic_pow_val(s2, p_numb=5))
+
+s3 = 0.270
+print(p_adic.p_adic_pow_val(s3, p_numb=3))
+
+s4 = 0.270
+print(p_adic.p_adic_pow_val(s4, p_numb=5))
 
 # p-parameter of p_adic_val() function must be prime!
 m = 16
 print(p_adic.p_adic_val(m, p_num=4))
-
-# p-adic Distance
-
-import numpy as np
-
-# Example - 1
-# create two numpy arrays
-a = np.array([1, 5, -20])
-b = np.array([1, 10, 20])
-
-dist_ab2 = p_adic.p_adic_dist(x_vec=a, y_vec=b, p_adic_p=2)
-print("2-adic distance between ", a, " and ", b, " is ", dist_ab2)
-
-dist_ab3 = p_adic.p_adic_dist(x_vec=a, y_vec=b, p_adic_p=3)
-print("3-adic distance between ", a, " and ", b, " is ", dist_ab3)
-
-dist_ab5 = p_adic.p_adic_dist(x_vec=a, y_vec=b, p_adic_p=5)
-print("5-adic distance between ", a, " and ", b, " is ", dist_ab5)
-
-dist_ab7 = p_adic.p_adic_dist(x_vec=a, y_vec=b, p_adic_p=7)
-print("7-adic distance between ", a, " and ", b, " is ", dist_ab7)
-
-dist_ab11 = p_adic.p_adic_dist(x_vec=a, y_vec=b, p_adic_p=11)
-print("11-adic distance between ", a, " and ", b, " is ", dist_ab11)
-
-dist_ab29 = p_adic.p_adic_dist(x_vec=a, y_vec=b, p_adic_p=29)
-print("29-adic distance between ", a, " and ", b, " is ", dist_ab29)
-
-# Example - 2
-# create two numpy arrays
-np.set_printoptions(suppress=True)
-
-c = np.array([0.1, 50, 101.2, -23])
-d = np.array([0.028, -40, 150.20, 35])
-
-dist_cd2 = p_adic.p_adic_dist(x_vec=c, y_vec=d, p_adic_p=2)
-print("2-adic distance between ", c, " and ", d, " is ", dist_cd2)
-
-dist_cd3 = p_adic.p_adic_dist(x_vec=c, y_vec=d, p_adic_p=3)
-print("3-adic distance between ", c, " and ", d, " is ", dist_cd3)
-
-dist_cd5 = p_adic.p_adic_dist(x_vec=c, y_vec=d, p_adic_p=5)
-print("5-adic distance between ", c, " and ", d, " is ", dist_cd5)
-
-dist_cd7 = p_adic.p_adic_dist(x_vec=c, y_vec=d, p_adic_p=7)
-print("7-adic distance between ", c, " and ", d, " is ", dist_cd7)
-
-dist_cd11 = p_adic.p_adic_dist(x_vec=c, y_vec=d, p_adic_p=11)
-print("11-adic distance between ", c, " and ", d, " is ", dist_cd11)
-
-dist_cd29 = p_adic.p_adic_dist(x_vec=c, y_vec=d, p_adic_p=29)
-print("29-adic distance between ", c, " and ", d, " is ", dist_cd29)
-
-
-
-
-
-
-
-
-
